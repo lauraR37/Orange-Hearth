1990b6600b0e8c6a93f2a57ad0eb15a16105fa7e
Laura Sofía Rivera Cardozo <tatav.71397@gmail.com>
2025-11-02 20:53:42 -0500

    Feature: Implementado WelcomeController y verificación de arranque

---
+++ src/main/java/Orange/Hearth/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package Orange.Hearth.controller; 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 1. Indica que es un controlador REST
+public class WelcomeController {
+
+    // 2. Mapea peticiones GET a la URL /welcome
+    @GetMapping("/welcome") 
+    public String sayWelcome() {
+        return "¡Bienvenido a Orange Hearth! El controlador funciona.";
+    }
+}
