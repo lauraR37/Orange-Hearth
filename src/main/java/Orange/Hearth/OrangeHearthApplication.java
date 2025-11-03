3bd0003820a49eb8e970f7f0951d50b02bc0d4bf
Laura Sofía Rivera Cardozo <tatav.71397@gmail.com>
2025-11-02 20:11:26 -0500

    Commit inicial: Estructura base de Orange Hearth

---
+++ src/main/java/Orange/Hearth/OrangeHearthApplication.java
@@ -0,0 +1,13 @@
+package Orange.Hearth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeHearthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeHearthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Orange/Hearth/OrangeHearthApplicationTests.java
@@ -0,0 +1,13 @@
+package Orange.Hearth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeHearthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
